12ef973fd2f925e7632c184cc3ffe69812c0441f
Tom Banish <tombanish@gmail.com>
2020-06-08 15:35:29 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "male", "brown", "calm", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "male", "grey", "feisty", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7, "female", "brown", "gentle", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "female", "brown", "tough", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 9, "female", "black", "sweet", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 4, "male", "grey", "mean", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "female", "black", "friendly", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, NULL, "brown", "angry", "false");
